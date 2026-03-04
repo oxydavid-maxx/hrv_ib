a1fed7a3727b4a77b29b06c7e60cb7c9373ff9bc
Kuang-Yu [光佑] <kuangyu@realtek.com>
2026-03-04 16:38:50 +0800

    Add missing androidTest rules dependency
    Include androidx.test:rules for GrantPermissionRule in instrumentation tests to prevent compileDebugAndroidTestKotlin unresolved reference failures.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,6 +102,7 @@ dependencies {
     testImplementation("org.robolectric:robolectric:4.13")
 
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test:rules:1.6.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     androidTestImplementation("com.google.dagger:hilt-android-testing:2.52")
